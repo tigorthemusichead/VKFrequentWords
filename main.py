ef8bcf476deeb52850c255b22527d254b946ccf9
tigorthemusichead <92551370+tigorthemusichead@users.noreply.github.com>
2022-02-04 00:25:32 +0300

    Update main.py
    split

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-# <div class="im-mess--text wall_module _im_log_body">И возвращаюсь</div>
-
-f = open('chat.html', 'r')
-text = f.read()
-words = []
-
-position = text.find('<div class="im-mess--text wall_module _im_log_body">')
-while text and position != -1:
-    text = text[position+52:]
-    check_pose = text.find('<')
-    position = text.find('</div>')
-    if position == check_pose:
-        words.append(text[:position])
-    position = text.find('<div class="im-mess--text wall_module _im_log_body">')
-
-print(words)
-
+#подключение файла make.py
+import make_vk
+#генерация картинки
+make_vk.make()
