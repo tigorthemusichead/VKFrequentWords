09cf60722a59595a9e4b17d0828c60d1fab65a7f
tigorthemusichead <92551370+tigorthemusichead@users.noreply.github.com>
2022-02-03 22:44:41 +0300

    Create main.py
    create

---
+++ main.py
@@ -0,0 +1,17 @@
+# <div class="im-mess--text wall_module _im_log_body">И возвращаюсь</div>
+
+f = open('chat.html', 'r')
+text = f.read()
+words = ''
+
+position = text.find('<div class="im-mess--text wall_module _im_log_body">')
+while text and position != -1:
+    text = text[position+52:]
+    check_pose = text.find('<')
+    position = text.find('</div>')
+    if position == check_pose:
+        words += text[:position] + ' '
+    position = text.find('<div class="im-mess--text wall_module _im_log_body">')
+
+print(words)
+
